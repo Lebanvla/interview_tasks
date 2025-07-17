99df642f60495077c29ce08a821b16c4458d9fa5
Lebanvla <antonlebedev2004@yandex.ru>
2025-07-18 01:48:40 +0300

    Улучшил код

--- task_1.php
+++ task_1.php
@@ -1,12 +1,8 @@
 <?php
+    $title = "Задание 1";
     include("./logic/task_1.php");
+    include("./templates/header.php");
 ?>
-<head>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
-    <title>
-    Задание 1
-    </title>
-</head>
 <body class="container">
 <h3>
     Результат работы:
